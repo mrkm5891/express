8738af669cddf8ef84d8099b101da2d2020f8c9f
mrkm5891 <2101130226kk@cyber-u.ac.jp>
2024-07-26 23:31:43 +0900

    add env

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
-var YesNoRouter = require('./routes/YesNo');
+var yesnoRouter = require('./routes/yesno');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -28,7 +29,8 @@ app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
-app.use('/YesNo', YesNoRouter);
+app.use('/yesno', yesnoRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
